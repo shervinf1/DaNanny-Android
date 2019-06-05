cd5a0ac98abab2985e183c56003180a0824a8fec
jlcruza <jlcruz7125@gmail.com>
2019-06-05 12:58:04 -0400

    Able to Re-Draw the Graph

--- app/src/main/java/com/example/dananny/MainActivity.java
+++ app/src/main/java/com/example/dananny/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.dananny;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.CountDownTimer;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -18,6 +24,7 @@ import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.Utils;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +37,7 @@ int fillColor = Color.argb(150,38, 166, 154);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Graph Settings
         lineChart = findViewById(R.id.lineChart);
         lineChart.setBackgroundColor(Color.WHITE);
         lineChart.setGridBackgroundColor(Color.WHITE);
@@ -39,6 +47,7 @@ int fillColor = Color.argb(150,38, 166, 154);
         lineChart.setPinchZoom(true);
         lineChart.getLegend().setEnabled(false);
 
+        //Graph Axis Settings
         lineChart.getAxisRight().setEnabled(false);
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         lineChart.getXAxis().setDrawAxisLine(false);
@@ -48,7 +57,25 @@ int fillColor = Color.argb(150,38, 166, 154);
         lineChart.getAxisLeft().setAxisMaximum(26);
         lineChart.getAxisLeft().setAxisMinimum(0);
 
-        setData(10,25);
+        //Refresh button
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                //Get Random Data Points
+                setData(10, 25);
+
+                //Re-Draw the graph with new data
+                lineChart.invalidate();
+            }
+        });
+
+
+    }
+
+    private  void CreateData(){
+
     }
 
     private void setData(int count, float range){
@@ -61,38 +88,47 @@ int fillColor = Color.argb(150,38, 166, 154);
         }
 
 
-        LineDataSet set1, set2;
+        LineDataSet set1;
 
-        set1 = new LineDataSet(yVals, "Data Set1");
-        set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set1.setColor(Color.rgb(0, 188, 212));
-        set1.setDrawCircles(true);
-        set1.setCircleColor(Color.rgb(40, 180, 99));
-        set1.setLineWidth(4);
-        set1.setFillAlpha(255);
-        set1.setCircleRadius(4f);
-        set1.setDrawCircleHole(false);
-        set1.setHighlightEnabled(false);
-
-        // set the filled area
-        set1.setDrawFilled(true);
-        set1.setFillFormatter(new IFillFormatter() {
-            @Override
-            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                return lineChart.getAxisLeft().getAxisMinimum();
-            }
-        });
-
-        // set color of filled area
-        if (Utils.getSDKInt() >= 18) {
-            // drawables only supported on api level 18 and above
-            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
-            set1.setFillDrawable(drawable);
+        if (lineChart.getData() != null &&
+                lineChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) lineChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals);
+            set1.notifyDataSetChanged();
+            lineChart.getData().notifyDataChanged();
+            lineChart.notifyDataSetChanged();
         } else {
-            set1.setFillColor(Color.BLACK);
-        }
 
+            set1 = new LineDataSet(yVals, "Data Set1");
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(Color.rgb(0, 188, 212));
+            set1.setDrawCircles(true);
+            set1.setCircleColor(Color.rgb(40, 180, 99));
+            set1.setLineWidth(4);
+            set1.setFillAlpha(255);
+            set1.setCircleRadius(4f);
+            set1.setDrawCircleHole(false);
+            set1.setHighlightEnabled(false);
+
+            // set the filled area
+            set1.setDrawFilled(true);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return lineChart.getAxisLeft().getAxisMinimum();
+                }
+            });
+
+            // set color of filled area
+            if (Utils.getSDKInt() >= 18) {
+                // drawables only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.BLACK);
+            }
+        }
         LineData data = new LineData(set1);
         data.setDrawValues(false);
 
