f58d3035278873ec91fe9db247d6f8bff7c5cd68
jlcruza <jlcruz7125@gmail.com>
2019-08-29 12:03:44 -0400

    Improved LoginPage design

--- app/src/main/java/com/example/dananny/Login.java
+++ app/src/main/java/com/example/dananny/Login.java
@@ -3,11 +3,15 @@ package com.example.dananny;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +27,8 @@ public class Login extends AppCompatActivity {
     EditText Password;
     TextView Switcher;
     Button SignButton;
+    LinearLayout SigningPage;
+    LinearLayout UserKey;
     final String haveAccountString = "Already have an account?";
     final String noAccountString = "Don't have an account?";
     private boolean isNewUser = false;
@@ -48,7 +54,8 @@ public class Login extends AppCompatActivity {
         Username = findViewById(R.id.editTextEmail);
         Password = findViewById(R.id.editTextPassword);
         Switcher = findViewById(R.id.credentialSwitch);
-
+        SigningPage = findViewById(R.id.SigningPageLayout);
+        UserKey = findViewById(R.id.setUserKeyLayout);
 
         SignButton = findViewById(R.id.BtnSignIn);
         SignButton.setOnClickListener(new View.OnClickListener() {
@@ -99,6 +106,28 @@ public class Login extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Log.d("LoginPage", "SUCCESS");
                             FirebaseUser user = mAuth.getCurrentUser();
+
+                            DisplayMetrics displayMetrics = new DisplayMetrics();
+                            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                            int width = displayMetrics.widthPixels;
+
+                            Animation moveLeft = new TranslateAnimation(0,-width,0,0);
+                            moveLeft.setDuration(500);
+                            moveLeft.setFillAfter(true);
+
+                            Animation moveRight = new TranslateAnimation(0,width,0,0);
+                            moveRight.setDuration(100);
+                            moveRight.setFillAfter(true);
+
+                            Animation returnCenter = new TranslateAnimation(0,0,0,0);
+                            returnCenter.setDuration(100);
+                            returnCenter.setFillAfter(true);
+
+                            UserKey.startAnimation(moveRight);
+                            SigningPage.setVisibility(View.GONE);
+                            SigningPage.startAnimation(moveLeft);
+                            UserKey.startAnimation(returnCenter);
+                            UserKey.setVisibility(View.VISIBLE);
                         } else{
                             Log.w("LoginPage", "LOGIN FAILED!", task.getException());
                             Toast.makeText(getApplicationContext(), "LOGIN FAILED!", Toast.LENGTH_SHORT).show();
