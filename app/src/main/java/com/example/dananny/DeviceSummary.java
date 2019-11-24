3fe9375b5d69e51a30e4e5eb69fd5ec97cc3f5d8
jlcruza <jlcruz7125@gmail.com>
2019-11-24 16:51:49 -0400

    Device Summary with event listener

--- app/src/main/java/com/example/dananny/DeviceSummary.java
+++ app/src/main/java/com/example/dananny/DeviceSummary.java
@@ -2,7 +2,6 @@ package com.example.dananny;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,7 +31,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -41,6 +41,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class DeviceSummary extends AppCompatActivity implements Serializable {
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -52,6 +54,7 @@ public class DeviceSummary extends AppCompatActivity implements Serializable {
     private static final String TAG = "DeviceSummary";
     private final int BY_HOUR = 1;
     private final int BY_DATE = 2;
+    private final int NONE = 2;
     LineChart lineChart;
     Button btnDelete;
     final Context context = this;
@@ -99,7 +102,9 @@ public class DeviceSummary extends AppCompatActivity implements Serializable {
                         consumptionText.setText(Float.toString(document.getConsumption()));
 
 
-                        getDeviceID(devGpio);
+
+                        //getDeviceID(devGpio);
+                        getConsumptionByID(doc.getId());
 
                     }
 
@@ -198,36 +203,39 @@ public class DeviceSummary extends AppCompatActivity implements Serializable {
 
         db.collection("Measurements")
                 .whereEqualTo("deviceID", document)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                .limit(5)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                                measurements.add(documentSnapshot.toObject(Measurements.class));
-                                Log.d("Data", documentSnapshot.toString());
-                            }
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
 
-                            //Sort in Chronological Order
-                            Collections.reverse(measurements);
+                        measurements.clear();
+                        values.clear();
+                        timeManagers.clear();
 
-                            //Creates timemanagers for grouping
-                            for (Measurements data : measurements) {
-                                timeManagers.add(new TimeManager(data.getDate()));
-                            }
-
-                            //Group by Hour
-                            //List<Measurements> list = measurementGroupBy(measurements, timeManagers, BY_HOUR);
-
-                            //Creates timemanagers for grouping
-                            int counter = 0;
-                            for (Measurements data : measurements) {
-                                values.add(new Entry(counter, data.getWatts()));
-                                counter++;
-                            }
-
-                            setData(values, timeManagers);
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            measurements.add(documentSnapshot.toObject(Measurements.class));
+                            Log.d("Data", documentSnapshot.toString());
                         }
+
+                        //Sort in Chronological Order
+                        Collections.reverse(measurements);
+
+                        //Creates timemanagers for grouping
+                        for (Measurements data : measurements) {
+                            timeManagers.add(new TimeManager(data.getDate()));
+                        }
+
+                        //Group by Hour
+                        //List<Measurements> list = measurementGroupBy(measurements, timeManagers, BY_HOUR);
+
+                        //Creates timemanagers for grouping
+                        int counter = 0;
+                        for (Measurements data : measurements) {
+                            values.add(new Entry(counter, data.getWatts()));
+                            counter++;
+                        }
+
+                        setData(values, timeManagers);
                     }
                 });
     }
@@ -271,113 +279,121 @@ public class DeviceSummary extends AppCompatActivity implements Serializable {
         List<Measurements> measureDummy = new ArrayList<>();
         List<TimeManager> timeDummy = new ArrayList<>();
 
-        TimeManager timeBefore = timeManagerList.get(0);
-        Measurements dummy = measurementsList.get(0);
-        measureDummy.add(dummy);
-
-        if (groupCode == BY_HOUR) {
+        if(measurementsList.size() > 0 && timeManagerList.size() > 0) {
 
 
-            for (int i = 1; i < timeManagerList.size(); i++) {
-                System.out.println("Entered the for loop: " + i);
-                System.out.println("dummy: " + dummy.getDate());
+            TimeManager timeBefore = timeManagerList.get(0);
+            Measurements dummy = measurementsList.get(0);
+            measureDummy.add(dummy);
 
-                //Check if same year
-                if (timeBefore.getYear() == timeManagerList.get(i).getYear()) {
-                    //Check if same month
-                    if (timeBefore.getMonth() == timeManagerList.get(i).getMonth()) {
-                        //Check if same day of the month
-                        if (timeBefore.getDay() == timeManagerList.get(i).getDay()) {
-                            //Check if same hour
-                            if (timeBefore.getHour() == timeManagerList.get(i).getHour()) {
+            if (groupCode == BY_HOUR) {
 
-                                //Sum consumption
-                                System.out.println("Summing" + i);
-                                dummy.setCurrent(dummy.getCurrent() + measurementsList.get(i).getCurrent());
-                                dummy.setVoltage(dummy.getVoltage() + measurementsList.get(i).getVoltage());
-                                dummy.setWatts(dummy.getWatts() + measurementsList.get(i).getWatts());
 
+                for (int i = 1; i < timeManagerList.size(); i++) {
+                    System.out.println("Entered the for loop: " + i);
+                    System.out.println("dummy: " + dummy.getDate());
+
+                    //Check if same year
+                    if (timeBefore.getYear() == timeManagerList.get(i).getYear()) {
+                        //Check if same month
+                        if (timeBefore.getMonth() == timeManagerList.get(i).getMonth()) {
+                            //Check if same day of the month
+                            if (timeBefore.getDay() == timeManagerList.get(i).getDay()) {
+                                //Check if same hour
+                                if (timeBefore.getHour() == timeManagerList.get(i).getHour()) {
+
+                                    //Sum consumption
+                                    System.out.println("Summing" + i);
+                                    dummy.setCurrent(dummy.getCurrent() + measurementsList.get(i).getCurrent());
+                                    dummy.setVoltage(dummy.getVoltage() + measurementsList.get(i).getVoltage());
+                                    dummy.setWatts(dummy.getWatts() + measurementsList.get(i).getWatts());
+
+                                } else {
+                                    //Not same Hour
+                                    System.out.println("Not same hour" + i);
+                                    measureDummy.add(measurementsList.get(i));
+                                    timeDummy.add(timeManagerList.get(i));
+                                    dummy = measurementsList.get(i);
+                                }
                             } else {
-                                //Not same Hour
-                                System.out.println("Not same hour" + i);
+                                //Not same Day
+                                System.out.println("Not same day" + i);
                                 measureDummy.add(measurementsList.get(i));
                                 timeDummy.add(timeManagerList.get(i));
                                 dummy = measurementsList.get(i);
                             }
                         } else {
-                            //Not same Day
-                            System.out.println("Not same day" + i);
+                            //Not same Month
+                            System.out.println("Not same month" + i);
                             measureDummy.add(measurementsList.get(i));
                             timeDummy.add(timeManagerList.get(i));
                             dummy = measurementsList.get(i);
                         }
                     } else {
-                        //Not same Month
-                        System.out.println("Not same month" + i);
+                        //Not same Year
+                        System.out.println("Not same year" + i);
                         measureDummy.add(measurementsList.get(i));
                         timeDummy.add(timeManagerList.get(i));
                         dummy = measurementsList.get(i);
                     }
-                } else {
-                    //Not same Year
-                    System.out.println("Not same year" + i);
-                    measureDummy.add(measurementsList.get(i));
-                    timeDummy.add(timeManagerList.get(i));
-                    dummy = measurementsList.get(i);
+
+                    timeBefore = timeManagerList.get(i);
+
                 }
+                timeManagerList = timeDummy;
+                System.out.println("List size" + measureDummy.size());
+                return measureDummy;
 
-                timeBefore = timeManagerList.get(i);
+            } else if (groupCode == BY_DATE) {
 
-            }
-            timeManagerList = timeDummy;
-            System.out.println("List size" + measureDummy.size());
-            return measureDummy;
+                for (int i = 1; i < timeManagerList.size(); i++) {
 
-        } else if (groupCode == BY_DATE) {
+                    //Check if same year
+                    if (timeBefore.getYear() == timeManagerList.get(i).getYear()) {
+                        //Check if same month
+                        if (timeBefore.getMonth() == timeManagerList.get(i).getMonth()) {
+                            //Check if same day of the month
+                            if (timeBefore.getDay() == timeManagerList.get(i).getDay()) {
 
-            for (int i = 1; i < timeManagerList.size(); i++) {
-
-                //Check if same year
-                if (timeBefore.getYear() == timeManagerList.get(i).getYear()) {
-                    //Check if same month
-                    if (timeBefore.getMonth() == timeManagerList.get(i).getMonth()) {
-                        //Check if same day of the month
-                        if (timeBefore.getDay() == timeManagerList.get(i).getDay()) {
-
-                            //Sum consumption
-                            dummy.setCurrent(dummy.getCurrent() + measurementsList.get(i).getCurrent());
-                            dummy.setVoltage(dummy.getVoltage() + measurementsList.get(i).getVoltage());
-                            dummy.setWatts(dummy.getWatts() + measurementsList.get(i).getWatts());
+                                //Sum consumption
+                                dummy.setCurrent(dummy.getCurrent() + measurementsList.get(i).getCurrent());
+                                dummy.setVoltage(dummy.getVoltage() + measurementsList.get(i).getVoltage());
+                                dummy.setWatts(dummy.getWatts() + measurementsList.get(i).getWatts());
+                            } else {
+                                //Not same Day
+                                System.out.println("Not same day" + i);
+                                measureDummy.add(measurementsList.get(i));
+                                timeDummy.add(timeManagerList.get(i));
+                                dummy = measurementsList.get(i);
+                            }
                         } else {
-                            //Not same Day
-                            System.out.println("Not same day" + i);
+                            //Not same Month
+                            System.out.println("Not same month" + i);
                             measureDummy.add(measurementsList.get(i));
                             timeDummy.add(timeManagerList.get(i));
                             dummy = measurementsList.get(i);
                         }
                     } else {
-                        //Not same Month
-                        System.out.println("Not same month" + i);
+                        //Not same Year
+                        System.out.println("Not same year" + i);
                         measureDummy.add(measurementsList.get(i));
                         timeDummy.add(timeManagerList.get(i));
                         dummy = measurementsList.get(i);
                     }
-                } else {
-                    //Not same Year
-                    System.out.println("Not same year" + i);
-                    measureDummy.add(measurementsList.get(i));
-                    timeDummy.add(timeManagerList.get(i));
-                    dummy = measurementsList.get(i);
+
+                    timeBefore = timeManagerList.get(i);
+
                 }
 
-                timeBefore = timeManagerList.get(i);
+                return measureDummy;
 
+            } else if (groupCode == NONE) {
+                return measurementsList;
+            } else {
+                return measurementsList;
             }
-
-            return measureDummy;
-
         } else {
-            return null;
+            return measurementsList;
         }
     }
 
